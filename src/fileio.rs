8d528859559468d8843d3eb9e640922128d157dd
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-08-15 17:54:32 +1000

    Fully implement run command

--- src/fileio.rs
+++ src/fileio.rs
@@ -3,19 +3,41 @@ use std::fs;
 use std::path::Path;
 use std::process::Command;
 
-fn run(filename: &str) -> std::io::Result<()> {
+fn run(python_commands: &str, filename: &str) -> std::io::Result<String> {
     // create directory
     fs::create_dir_all(DIR_TEMPORARY)?;
 
+    // combine header with commands
+    let mut contents = String::new();
+    contents.push_str(PYTHON_HEADER);
+    contents.push_str(python_commands);
+
     // write file
     let filepath = Path::new(DIR_TEMPORARY).join(filename);
-    fs::write(&filepath, PYTHON_HEADER)?;
+    fs::write(&filepath, contents)?;
 
     // execute file
-    Command::new("python3").arg(&filepath).output()?;
+    let output = Command::new("python3").arg(&filepath).output()?;
+
+    // results
+    let out = match String::from_utf8(output.stdout) {
+        Ok(v) => v,
+        Err(e) => format!("ERROR: cannot convert command line stdout\n{}", e),
+    };
+    let err = match String::from_utf8(output.stderr) {
+        Ok(v) => v,
+        Err(e) => format!("ERROR: cannot convert command line stderr\n{}", e),
+    };
+    let mut results = String::new();
+    if out.len() > 0 {
+        results.push_str(&out);
+    }
+    if err.len() > 0 {
+        results.push_str(&err)
+    }
 
     // done
-    Ok(())
+    Ok(results)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -27,14 +49,17 @@ mod tests {
 
     #[test]
     fn run_works() -> Result<(), Box<dyn std::error::Error>> {
-        run("generate.py")?;
+        let commands = "print(\"Python says: Hello World!\")".to_string();
+        let output = run(&commands, "generate.py")?;
         let entries = fs::read_dir(DIR_TEMPORARY)?
             .map(|res| res.map(|e| e.path()))
             .collect::<Result<Vec<_>, io::Error>>()?;
         assert_eq!(entries.len(), 1);
         let data = fs::read_to_string(Path::new(DIR_TEMPORARY).join("generate.py"))?;
-        let correct = String::from(PYTHON_HEADER);
+        let mut correct = String::from(PYTHON_HEADER);
+        correct.push_str(&commands);
         assert_eq!(data, correct);
+        assert_eq!(output, "Python says: Hello World!\n");
         Ok(())
     }
 }

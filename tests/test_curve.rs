fd25efcc7af811965f295bb2b3196789aea0b927
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-09-15 08:16:24 +1000

    Fix marker void in Curve

--- tests/test_curve.rs
+++ tests/test_curve.rs
@@ -8,29 +8,39 @@ const OUT_DIR: &str = "/tmp/plotpy/integration_tests";
 #[test]
 fn test_curve() -> Result<(), &'static str> {
     // curve object and options
-    let mut curve = Curve::new();
-    curve.line_alpha = 0.7;
-    curve.line_color = "#cd0000".to_string();
-    curve.line_style = "--".to_string();
-    curve.line_width = 2.0;
-    curve.marker_color = "#1862ab".to_string();
-    curve.marker_every = 2;
-    curve.marker_void = false;
-    curve.marker_line_color = "#cda500".to_string();
-    curve.marker_line_width = 3.0;
-    curve.marker_size = 8.0;
-    curve.marker_style = "p".to_string();
+    let mut curve1 = Curve::new();
+    curve1.line_alpha = 0.7;
+    curve1.line_color = "#cd0000".to_string();
+    curve1.line_style = "--".to_string();
+    curve1.line_width = 2.0;
+    curve1.marker_color = "#1862ab".to_string();
+    curve1.marker_every = 2;
+    curve1.marker_void = false;
+    curve1.marker_line_color = "#cda500".to_string();
+    curve1.marker_line_width = 3.0;
+    curve1.marker_size = 8.0;
+    curve1.marker_style = "p".to_string();
 
-    // draw curve
+    // another curve
+    let mut curve2 = Curve::new();
+    curve2.line_style = "None".to_string();
+    curve2.marker_line_color = "#1862ab".to_string();
+    curve2.marker_style = "s".to_string();
+    curve2.marker_void = true;
+
+    // draw curves
     let x = &[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0];
     let y = &[
         1.0, 1.41421356, 1.73205081, 2.0, 2.23606798, 2.44948974, 2.64575131, 2.82842712, 3.0, 3.16227766,
     ];
-    curve.draw(x, y);
+    let y2 = &[1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 3.5, 3.5, 3.5, 3.5];
+    curve1.draw(x, y);
+    curve2.draw(x, y2);
 
     // add contour to plot
     let mut plot = Plot::new();
-    plot.add(&curve);
+    plot.add(&curve1);
+    plot.add(&curve2);
 
     // save figure
     let path = Path::new(OUT_DIR).join("curve.svg");
@@ -40,6 +50,6 @@ fn test_curve() -> Result<(), &'static str> {
     let file = File::open(path).map_err(|_| "cannot open file")?;
     let buffered = BufReader::new(file);
     let lines_iter = buffered.lines();
-    assert_eq!(lines_iter.count(), 474);
+    assert_eq!(lines_iter.count(), 511);
     Ok(())
 }

21300da89445bf0dec5f2b50253be1fbb4a1d845
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-09-15 14:09:52 +1000

    Improve Shapes doc

--- src/shapes.rs
+++ src/shapes.rs
@@ -2,6 +2,64 @@ use super::*;
 use std::fmt::Write;
 
 /// Draw polygonal shapes
+///
+/// # Example
+///
+/// ```
+/// # fn main() -> Result<(), &'static str> {
+/// // import
+/// use plotpy::*;
+/// use std::path::Path;
+///
+/// // directory to save figures
+/// const OUT_DIR: &str = "/tmp/plotpy/doc_tests";
+///
+/// // shapes object and options
+/// let mut shapes = Shapes::new();
+/// shapes.line_width = 3.0;
+/// shapes.edge_color = "#cd0000".to_string();
+/// shapes.face_color = "#eeea83".to_string();
+///
+/// // draw arc
+/// shapes.arc(0.5, 0.5, 0.4, 195.0, -15.0);
+///
+/// // draw arrow
+/// shapes.arrow_scale = 50.0;
+/// shapes.arrow_style = "fancy".to_string();
+/// shapes.arrow(0.4, 0.3, 0.6, 0.5);
+///
+/// // draw circle
+/// shapes.face_color = "None".to_string();
+/// shapes.edge_color = "#1f9c25".to_string();
+/// shapes.line_width = 6.0;
+/// shapes.circle(0.5, 0.5, 0.5);
+///
+/// // draw polyline
+/// shapes.line_width = 3.0;
+/// shapes.edge_color = "blue".to_string();
+/// let a = 0.2;
+/// let c = f64::sqrt(3.0) / 2.0;
+/// let p = vec![vec![0.1, 0.5], vec![0.1 + a, 0.5], vec![0.1 + a / 2.0, 0.5 + a * c]];
+/// let q = vec![vec![0.9, 0.5], vec![0.9 - a, 0.5], vec![0.9 - a / 2.0, 0.5 + a * c]];
+/// shapes.polyline(&p, true);
+/// shapes.polyline(&q, false);
+///
+/// // add shapes to plot
+/// let mut plot = Plot::new();
+/// plot.hide_axes();
+/// plot.equal();
+/// plot.range(-0.05, 1.05, -0.05, 1.05);
+/// plot.add(&shapes);
+///
+/// // save figure
+/// let path = Path::new(OUT_DIR).join("doc_shapes.svg");
+/// plot.save(&path)?;
+/// # Ok(())
+/// # }
+/// ```
+///
+/// ![doc_shapes.svg](https://raw.githubusercontent.com/cpmech/plotpy/main/figures/doc_shapes.svg)
+///
 pub struct Shapes {
     /// Edge color (shared)
     pub edge_color: String,

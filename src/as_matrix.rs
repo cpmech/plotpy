f927110a5ad98e9804b9dd147807fae388ffb4b6
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-09-18 18:33:59 +1000

    [wip] Add as_vector and as_matrix

---
+++ src/as_matrix.rs
@@ -0,0 +1,192 @@
+use russell_lab::Matrix;
+
+/// Defines a trait to handle Matrix-like data
+///
+/// # Example
+///
+/// ```
+/// use plotpy::AsMatrix;
+/// use russell_lab::Matrix;
+///
+/// fn sum<'a, T, U>(array: &'a T) -> f64
+/// where
+///     T: AsMatrix<'a, U>,
+///     U: 'a + Into<f64>,
+/// {
+///     let mut res = 0.0;
+///     let (m, n) = array.mat_size();
+///     for i in 0..m {
+///         for j in 0..n {
+///             res += array.mat_at(i, j).into();
+///         }
+///     }
+///     res
+/// }
+///
+/// // heap-allocated 2D array (vector of vectors)
+/// const IGNORED: f64 = 123.456;
+/// let a = vec![
+///     vec![1.0, 2.0],
+///     vec![3.0, 4.0, IGNORED, IGNORED, IGNORED],
+///     vec![5.0, 6.0],
+/// ];
+/// assert_eq!(sum(&a), 21.0);
+///
+/// // heap-allocated 2D array (aka slice of slices)
+/// let b: &[&[f64]] = &[
+///     &[10.0, 20.0],
+///     &[30.0, 40.0, IGNORED],
+///     &[50.0, 60.0, IGNORED, IGNORED],
+/// ];
+/// assert_eq!(sum(&b), 210.0);
+///
+/// // stack-allocated (fixed-size) 2D array
+/// let c = [
+///     [100.0, 200.0],
+///     [300.0, 400.0],
+///     [500.0, 600.0],
+/// ];
+/// assert_eq!(sum(&c), 2100.0);
+///
+/// // using Matrix
+/// let d = Matrix::from(&[
+///     [0.1, 0.2],
+///     [0.3, 0.4],
+///     [0.5, 0.6],
+/// ]);
+/// assert_eq!(sum(&d), 2.1);
+/// ```
+pub trait AsMatrix<'a, U: 'a> {
+    /// Returns the size of the matrix
+    fn mat_size(&self) -> (usize, usize);
+
+    /// Returns the value at index i
+    fn mat_at(&self, i: usize, j: usize) -> U;
+}
+
+/// Defines a heap-allocated 2D array (vector of vectors)
+///
+/// # Notes
+///
+/// * The number of columns is defined by the first row
+/// * The next rows must have at least the same number of columns as the first row
+impl<'a, U: 'a> AsMatrix<'a, U> for Vec<Vec<U>>
+where
+    U: 'a + Copy,
+{
+    fn mat_size(&self) -> (usize, usize) {
+        (self.len(), self[0].len())
+    }
+    fn mat_at(&self, i: usize, j: usize) -> U {
+        self[i][j]
+    }
+}
+
+/// Defines a heap-allocated 2D array (slice of slices)
+///
+/// # Notes
+///
+/// * The number of columns is defined by the first row
+/// * The next rows must have at least the same number of columns as the first row
+impl<'a, U> AsMatrix<'a, U> for &'a [&'a [U]]
+where
+    U: 'a + Copy,
+{
+    fn mat_size(&self) -> (usize, usize) {
+        (self.len(), self[0].len())
+    }
+    fn mat_at(&self, i: usize, j: usize) -> U {
+        self[i][j]
+    }
+}
+
+/// Defines a stack-allocated (fixed-size) 2D array
+impl<'a, U, const M: usize, const N: usize> AsMatrix<'a, U> for [[U; N]; M]
+where
+    U: 'a + Copy,
+{
+    fn mat_size(&self) -> (usize, usize) {
+        (self.len(), self[0].len())
+    }
+    fn mat_at(&self, i: usize, j: usize) -> U {
+        self[i][j]
+    }
+}
+
+/// Handles Matrix
+impl<'a> AsMatrix<'a, f64> for Matrix {
+    fn mat_size(&self) -> (usize, usize) {
+        self.dims()
+    }
+    fn mat_at(&self, i: usize, j: usize) -> f64 {
+        self.get(i, j)
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fmt::Write;
+
+    fn matrix_str<'a, T, U>(array: &'a T) -> String
+    where
+        T: AsMatrix<'a, U>,
+        U: 'a + std::fmt::Display,
+    {
+        let mut buf = String::new();
+        let (m, n) = array.mat_size();
+        for i in 0..m {
+            for j in 0..n {
+                write!(&mut buf, "{},", array.mat_at(i, j)).unwrap();
+            }
+            write!(&mut buf, "\n").unwrap();
+        }
+        buf
+    }
+
+    #[test]
+    fn as_matrix_works() {
+        // heap-allocated 2D array (vector of vectors)
+        const IGNORED: f64 = 123.456;
+        let a = vec![
+            vec![1.0, 2.0],
+            vec![3.0, 4.0, IGNORED, IGNORED, IGNORED],
+            vec![5.0, 6.0],
+        ];
+        assert_eq!(
+            matrix_str(&a),
+            "1,2,\n\
+             3,4,\n\
+             5,6,\n"
+        );
+
+        // heap-allocated 2D array (aka slice of slices)
+        let b: &[&[f64]] = &[&[10.0, 20.0], &[30.0, 40.0, IGNORED], &[50.0, 60.0, IGNORED, IGNORED]];
+        assert_eq!(
+            matrix_str(&b),
+            "10,20,\n\
+             30,40,\n\
+             50,60,\n"
+        );
+
+        // stack-allocated (fixed-size) 2D array
+        let c = [[100.0, 200.0], [300.0, 400.0], [500.0, 600.0]];
+        assert_eq!(
+            matrix_str(&c),
+            "100,200,\n\
+             300,400,\n\
+             500,600,\n"
+        );
+
+        // matrix
+        let d = Matrix::from(&[[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]]);
+        assert_eq!(
+            matrix_str(&d),
+            "0.1,0.2,\n\
+             0.3,0.4,\n\
+             0.5,0.6,\n"
+        );
+    }
+}
---
+++ src/as_vector.rs
@@ -0,0 +1,136 @@
+use russell_lab::Vector;
+
+/// Defines a trait to handle Vector-like data
+///
+/// # Example
+///
+/// ```
+/// use plotpy::AsVector;
+/// use russell_lab::Vector;
+///
+/// fn sum<'a, T, U>(array: &'a T) -> f64
+/// where
+///     T: AsVector<'a, U>,
+///     U: 'a + Into<f64>,
+/// {
+///     let mut res = 0.0;
+///     let m = array.vec_size();
+///     for i in 0..m {
+///         res += array.vec_at(i).into();
+///     }
+///     res
+/// }
+///
+/// // heap-allocated 1D array (vector)
+/// let x = vec![1.0, 2.0, 3.0];
+/// assert_eq!(sum(&x), 6.0);
+///
+/// // heap-allocated 1D array (slice)
+/// let y: &[f64] = &[10.0, 20.0, 30.0];
+/// assert_eq!(sum(&y), 60.0);
+///
+/// // stack-allocated (fixed-size) 2D array
+/// let z = [100.0, 200.0, 300.0];
+/// assert_eq!(sum(&z), 600.0);
+///
+/// // using Vector
+/// let w = Vector::from(&[5.0, 5.0, 5.0]);
+/// assert_eq!(sum(&w), 15.0);
+/// ```
+pub trait AsVector<'a, U: 'a> {
+    /// Returns the size of the vector
+    fn vec_size(&self) -> usize;
+
+    /// Returns the value at index i
+    fn vec_at(&self, i: usize) -> U;
+}
+
+/// Defines a heap-allocated 1D array (vector)
+impl<'a, U: 'a> AsVector<'a, U> for Vec<U>
+where
+    U: 'a + Copy,
+{
+    fn vec_size(&self) -> usize {
+        self.len()
+    }
+    fn vec_at(&self, i: usize) -> U {
+        self[i]
+    }
+}
+
+/// Defines a heap-allocated 1D array (slice)
+impl<'a, U> AsVector<'a, U> for &'a [U]
+where
+    U: 'a + Copy,
+{
+    fn vec_size(&self) -> usize {
+        self.len()
+    }
+    fn vec_at(&self, i: usize) -> U {
+        self[i]
+    }
+}
+
+/// Defines a stack-allocated (fixed-size) 1D array
+impl<'a, U, const M: usize> AsVector<'a, U> for [U; M]
+where
+    U: 'a + Copy,
+{
+    fn vec_size(&self) -> usize {
+        self.len()
+    }
+    fn vec_at(&self, i: usize) -> U {
+        self[i]
+    }
+}
+
+/// Handles Vector
+impl<'a> AsVector<'a, f64> for Vector {
+    fn vec_size(&self) -> usize {
+        self.dim()
+    }
+    fn vec_at(&self, i: usize) -> f64 {
+        self.get(i)
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fmt::Write;
+
+    fn vector_str<'a, T, U>(array: &'a T) -> String
+    where
+        T: AsVector<'a, U>,
+        U: 'a + std::fmt::Display,
+    {
+        let mut buf = String::new();
+        let m = array.vec_size();
+        for i in 0..m {
+            write!(&mut buf, "{},", array.vec_at(i)).unwrap();
+        }
+        write!(&mut buf, "\n").unwrap();
+        buf
+    }
+
+    #[test]
+    fn as_vector_works() {
+        // heap-allocated 1D array (vector)
+        let x = vec![1.0, 2.0, 3.0];
+        assert_eq!(vector_str(&x), "1,2,3,\n");
+
+        // heap-allocated 1D array (slice)
+        let y: &[f64] = &[10.0, 20.0, 30.0];
+        assert_eq!(vector_str(&y), "10,20,30,\n");
+
+        // stack-allocated (fixed-size) 2D array
+        let z = [100.0, 200.0, 300.0];
+        assert_eq!(vector_str(&z), "100,200,300,\n");
+
+        // vector
+        let w = Vector::from(&[10.0, 10.0, 10.0]);
+        assert_eq!(vector_str(&w), "10,10,10,\n");
+    }
+}

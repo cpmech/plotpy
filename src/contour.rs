114bb2198b8ac2c0c0aac19b09be23d4ab319571
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-09-18 18:56:50 +1000

    Add test

--- src/contour.rs
+++ src/contour.rs
@@ -303,6 +303,7 @@ impl GraphMaker for Contour {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use russell_lab::Matrix;
 
     #[test]
     fn new_works() {
@@ -435,6 +436,23 @@ mod tests {
                        cb=plt.colorbar(cf)\n\
                        cb.ax.set_ylabel(r'temperature')\n\
                        plt.contour(x,y,z,colors=['yellow'],levels=[0],linestyles=['-'],linewidths=[2])\n";
+        assert_eq!(contour.buffer, b);
+    }
+
+    #[test]
+    fn draw_with_matrix_works() {
+        let mut contour = Contour::new();
+        let x = Matrix::from(&[[-0.5, 0.0, 0.5], [-0.5, 0.0, 0.5], [-0.5, 0.0, 0.5]]);
+        let y = Matrix::from(&[[-0.5, -0.5, -0.5], [0.0, 0.0, 0.0], [0.5, 0.5, 0.5]]);
+        let z = Matrix::from(&[[0.5, 0.25, 0.5], [0.25, 0.0, 0.25], [0.5, 0.25, 0.5]]);
+        contour.draw(&x, &y, &z);
+        let b: &str = "x=np.array([[-0.5,0,0.5,],[-0.5,0,0.5,],[-0.5,0,0.5,],],dtype=float)\n\
+                       y=np.array([[-0.5,-0.5,-0.5,],[0,0,0,],[0.5,0.5,0.5,],],dtype=float)\n\
+                       z=np.array([[0.5,0.25,0.5,],[0.25,0,0.25,],[0.5,0.25,0.5,],],dtype=float)\n\
+                       cf=plt.contourf(x,y,z,cmap=getColormap(0))\n\
+                       cl=plt.contour(x,y,z,colors=['black'])\n\
+                       plt.clabel(cl,inline=True)\n\
+                       cb=plt.colorbar(cf)\n";
         assert_eq!(contour.buffer, b);
     }
 }

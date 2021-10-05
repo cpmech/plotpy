92ee1f2276157fdf8652054d60268cf989d6f0f3
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-10-06 09:45:36 +1000

    Rewrite SlopeIndicator

--- src/slope_indicator.rs
+++ src/slope_indicator.rs
@@ -1,19 +1,23 @@
-use super::{vector_to_array, GraphMaker};
+use super::GraphMaker;
 use std::fmt::Write;
 
 pub struct SlopeIndicator {
     flipped: bool,      // indicator is flipped
     log_x: bool,        // x-axis is logarithm
     log_y: bool,        // y-axis is logarithm
-    color: String,      // Color of lines and text
+    edge_color: String, // Color of indicator lines
+    face_color: String, // Color of indicator faces
     line_style: String, // Style of lines
     line_width: f64,    // Width of lines
+    length: f64,        // horizontal length of indicator in Axes coords
     offset_v: f64,      // vertical offset in points
     no_text: bool,      // do not draw text
     fontsize: f64,      // text font size
-    txt_slope: String,  // use fixed text for slope value
-    txt_offset_h: f64,  // horizontal offset for text in points
-    txt_offset_v: f64,  // vertical offset for text in points
+    precision: usize,   // precision of slope number in label
+    text_color: String, // Color of text
+    text_slope: String, // use fixed text for slope value
+    text_offset_h: f64, // horizontal offset for text in points
+    text_offset_v: f64, // vertical offset for text in points
     buffer: String,     // buffer
 }
 
@@ -24,46 +28,74 @@ impl SlopeIndicator {
             flipped: false,
             log_x: false,
             log_y: false,
-            color: "#3f3f3f".to_string(),
+            edge_color: "black".to_string(),
+            face_color: "#f3f3f3".to_string(),
             line_style: String::new(),
             line_width: 0.0,
+            length: 0.1,
             offset_v: 5.0,
             no_text: false,
             fontsize: 0.0,
-            txt_slope: String::new(),
-            txt_offset_h: 3.0,
-            txt_offset_v: 3.0,
+            precision: 0,
+            text_color: "black".to_string(),
+            text_slope: String::new(),
+            text_offset_h: 3.0,
+            text_offset_v: 3.0,
             buffer: String::new(),
         }
     }
 
     /// Draws an indicator of line slope
-    pub fn draw(&mut self, slope: f64, x_center: f64, y_center: f64, x_length: f64) {
-        // draw lines
-        let m = slope;
-        let l = x_length / 2.0;
+    pub fn draw(&mut self, slope: f64, x_center: f64, y_center: f64) {
+        // compute axis (normalized) coordinates and slope
         let (mut xc, mut yc) = (x_center, y_center);
-        let mut x = [xc - l, xc + l, xc + l, xc - l];
-        let mut y = [yc - m * l, yc - m * l, yc + m * l, yc - m * l];
-        if self.flipped {
-            x[1] = xc - l;
-            y[1] = yc + m * l;
-        }
+        let (mut xa, mut ya) = (xc + 1.0, yc + slope);
         if self.log_x {
-            for i in 0..4 {
-                x[i] = f64::powf(10.0, x[i]);
-            }
+            xc = f64::log10(xc);
+            xa = xc + 1.0;
         }
         if self.log_y {
-            for i in 0..4 {
-                y[i] = f64::powf(10.0, y[i]);
-            }
+            yc = f64::log10(yc);
+            ya = yc + slope;
         }
-        vector_to_array(&mut self.buffer, "x", &x);
-        vector_to_array(&mut self.buffer, "y", &y);
+        write!(
+            &mut self.buffer,
+            "xc,yc=dataToAxis(({},{}))\n\
+             xa,ya=dataToAxis(({},{}))\n\
+             m,l=(ya-yc)/(xa-xc),{}\n",
+            xc, yc, xa, ya, self.length,
+        )
+        .unwrap();
+
+        // set polygon
+        if self.flipped {
+            self.buffer.push_str(
+                "dat=[[pth.Path.MOVETO,(xc-l,yc-m*l)],\
+                      [pth.Path.LINETO,(xc-l,yc+m*l)],\
+                      [pth.Path.LINETO,(xc+l,yc+m*l)],\
+                      [pth.Path.CLOSEPOLY,(None,None)]]\n",
+            );
+        } else {
+            self.buffer.push_str(
+                "dat=[[pth.Path.MOVETO,(xc-l,yc-m*l)],\
+                      [pth.Path.LINETO,(xc+l,yc-m*l)],\
+                      [pth.Path.LINETO,(xc+l,yc+m*l)],\
+                      [pth.Path.CLOSEPOLY,(None,None)]]\n",
+            );
+        }
+
+        // draw
         let tf = self.transform(slope);
         let opt = self.options();
-        write!(&mut self.buffer, "{}plt.plot(x,y{})\n", &tf, &opt).unwrap();
+        write!(
+            &mut self.buffer,
+            "{}cmd,pts=zip(*dat)\n\
+             h=pth.Path(pts,cmd)\n\
+             p=pat.PathPatch(h{})\n\
+             plt.gca().add_patch(p)\n",
+            tf, opt,
+        )
+        .unwrap();
 
         // skip text
         if self.no_text {
@@ -71,31 +103,21 @@ impl SlopeIndicator {
         }
 
         // coordinates for labels
-        let mut xm = xc - l;
-        let mut xp = xc + l;
-        let mut ym = yc + m * l;
-        let mut yp = yc + m * l;
-        let mut yr = yc - m * l;
-        let mut ys = yc - m * l;
-        if self.log_x {
-            xc = f64::powf(10.0, xc);
-            xm = f64::powf(10.0, xm);
-            xp = f64::powf(10.0, xp);
-        }
-        if self.log_y {
-            yc = f64::powf(10.0, yc);
-            ym = f64::powf(10.0, ym);
-            yp = f64::powf(10.0, yp);
-            yr = f64::powf(10.0, yr);
-            ys = f64::powf(10.0, ys);
-        }
+        self.buffer.push_str(
+            "xm,ym=xc-l,yc-m*l\n\
+             xp,yp=xc+l,yc+m*l\n",
+        );
 
         // slope text
         let mut text = String::new();
-        if self.txt_slope == "" {
-            write!(&mut text, "'{}'", f64::abs(slope)).unwrap();
+        if self.text_slope == "" {
+            if self.precision == 0 {
+                write!(&mut text, "'{}'", f64::abs(slope)).unwrap();
+            } else {
+                write!(&mut text, "'{:.1$}'", f64::abs(slope), self.precision).unwrap();
+            }
         } else {
-            write!(&mut text, "{}", self.txt_slope).unwrap();
+            write!(&mut text, "{}", self.text_slope).unwrap();
         }
 
         // draw labels
@@ -104,46 +126,36 @@ impl SlopeIndicator {
         let (opt_x, opt_y) = self.options_text();
         if self.flipped {
             if slope < 0.0 {
-                write!(
-                    &mut self.buffer,
-                    "plt.text({},{},'1',ha='center',va='top'{})\n",
-                    xc, ym, opt_x
-                )
-                .unwrap();
+                write!(&mut self.buffer, "plt.text(xc,yp,'1',ha='center',va='top'{})\n", opt_x).unwrap();
             } else {
                 write!(
                     &mut self.buffer,
-                    "plt.text({},{},'1',ha='center',va='bottom'{})\n",
-                    xc, yp, opt_x
+                    "plt.text(xc,yp,'1',ha='center',va='bottom'{})\n",
+                    opt_x
                 )
                 .unwrap();
             }
             write!(
                 &mut self.buffer,
-                "plt.text({},{},{},ha='right',va='center'{})\n",
-                xm, yc, text, opt_y
+                "plt.text(xm,yc,{},ha='right',va='center'{})\n",
+                text, opt_y
             )
             .unwrap();
         } else {
             if slope < 0.0 {
                 write!(
                     &mut self.buffer,
-                    "plt.text({},{},'1',ha='center',va='bottom'{})\n",
-                    xc, yr, opt_x
+                    "plt.text(xc,ym,'1',ha='center',va='bottom'{})\n",
+                    opt_x
                 )
                 .unwrap();
             } else {
-                write!(
-                    &mut self.buffer,
-                    "plt.text({},{},'1',ha='center',va='top'{})\n",
-                    xc, ys, opt_x
-                )
-                .unwrap();
+                write!(&mut self.buffer, "plt.text(xc,ym,'1',ha='center',va='top'{})\n", opt_x).unwrap();
             }
             write!(
                 &mut self.buffer,
-                "plt.text({},{},{},ha='left',va='center'{})\n",
-                xp, yc, text, opt_y
+                "plt.text(xp,yc,{},ha='left',va='center'{})\n",
+                text, opt_y
             )
             .unwrap();
         }
@@ -167,9 +179,9 @@ impl SlopeIndicator {
         self
     }
 
-    /// Sets the color of lines and text
-    pub fn set_color(&mut self, color: &str) -> &mut Self {
-        self.color = String::from(color);
+    /// Sets the color of indicator lines
+    pub fn set_edge_color(&mut self, color: &str) -> &mut Self {
+        self.edge_color = String::from(color);
         self
     }
 
@@ -208,21 +220,27 @@ impl SlopeIndicator {
         self
     }
 
+    /// Sets the the precision of slope number in label
+    pub fn set_precision(&mut self, value: usize) -> &mut Self {
+        self.precision = value;
+        self
+    }
+
     /// Sets text of slope indicator
-    pub fn set_txt_slope(&mut self, slope: &str) -> &mut Self {
-        self.txt_slope = String::from(slope);
+    pub fn set_text_slope(&mut self, slope: &str) -> &mut Self {
+        self.text_slope = String::from(slope);
         self
     }
 
     /// Sets the horizontal offset for the text in normalized axes coordinates in points
-    pub fn set_txt_offset_h(&mut self, value: f64) -> &mut Self {
-        self.txt_offset_h = value;
+    pub fn set_text_offset_h(&mut self, value: f64) -> &mut Self {
+        self.text_offset_h = value;
         self
     }
 
     /// Sets the vertical offset for the text in normalized axes coordinates in points
-    pub fn set_txt_offset_v(&mut self, value: f64) -> &mut Self {
-        self.txt_offset_v = value;
+    pub fn set_text_offset_v(&mut self, value: f64) -> &mut Self {
+        self.text_offset_v = value;
         self
     }
 
@@ -237,10 +255,12 @@ impl SlopeIndicator {
             };
             write!(
                 &mut opt,
-                "tf=tra.offset_copy(plt.gca().transData,fig=plt.gcf(),x=0,y={},units='points')\n",
+                "tf=tra.offset_copy(plt.gca().transAxes,fig=plt.gcf(),x=0,y={},units='points')\n",
                 dv,
             )
             .unwrap();
+        } else {
+            opt.push_str("tf=plt.gca().transAxes\n");
         }
         opt
     }
@@ -248,74 +268,70 @@ impl SlopeIndicator {
     /// Returns the coordinate transform for text
     fn transform_text(&self, slope: f64) -> String {
         let mut opt = String::new();
-        if self.offset_v > 0.0 || self.txt_offset_v > 0.0 {
+        if self.offset_v > 0.0 || self.text_offset_v > 0.0 {
             let dv = if self.flipped {
-                (self.offset_v + self.txt_offset_v) * f64::signum(slope)
+                (self.offset_v + self.text_offset_v) * f64::signum(slope)
             } else {
-                -(self.offset_v + self.txt_offset_v) * f64::signum(slope)
+                -(self.offset_v + self.text_offset_v) * f64::signum(slope)
             };
             write!(
                 &mut opt,
-                "tfx=tra.offset_copy(plt.gca().transData,fig=plt.gcf(),x=0,y={},units='points')\n",
+                "tfx=tra.offset_copy(plt.gca().transAxes,fig=plt.gcf(),x=0,y={},units='points')\n",
                 dv,
             )
             .unwrap();
+        } else {
+            opt.push_str("tfx=plt.gca().transAxes\n");
         }
-        if self.offset_v > 0.0 || self.txt_offset_h > 0.0 {
+        if self.offset_v > 0.0 || self.text_offset_h > 0.0 {
             let dv = if self.flipped {
                 self.offset_v * f64::signum(slope)
             } else {
                 -self.offset_v * f64::signum(slope)
             };
             let dh = if self.flipped {
-                -self.txt_offset_h
+                -self.text_offset_h
             } else {
-                self.txt_offset_h
+                self.text_offset_h
             };
             write!(
                 &mut opt,
-                "tfy=tra.offset_copy(plt.gca().transData,fig=plt.gcf(),x={},y={},units='points')\n",
+                "tfy=tra.offset_copy(plt.gca().transAxes,fig=plt.gcf(),x={},y={},units='points')\n",
                 dh, dv,
             )
             .unwrap();
+        } else {
+            opt.push_str("tfy=plt.gca().transAxes\n");
         }
         opt
     }
 
     /// Returns options for slope indicator
     fn options(&self) -> String {
-        let mut opt = String::new();
-        if self.color != "" {
-            write!(&mut opt, ",color='{}'", self.color).unwrap();
+        let mut opt = String::from(",transform=tf");
+        if self.edge_color != "" {
+            write!(&mut opt, ",edgecolor='{}'", self.edge_color).unwrap();
         }
-        if self.line_style != "" {
-            write!(&mut opt, ",linestyle='{}'", self.line_style).unwrap();
+        if self.face_color != "" {
+            write!(&mut opt, ",facecolor='{}'", self.face_color).unwrap();
         }
         if self.line_width > 0.0 {
             write!(&mut opt, ",linewidth={}", self.line_width).unwrap();
-        }
-        if self.offset_v > 0.0 {
-            write!(&mut opt, ",transform=tf").unwrap();
         }
         opt
     }
 
     /// Returns options for text
     fn options_text(&self) -> (String, String) {
-        let mut opt_x = String::new();
-        let mut opt_y = String::new();
-        if self.color != "" {
-            write!(&mut opt_x, ",color='{}'", self.color).unwrap();
+        let mut opt_x = String::from(",transform=tfx");
+        let mut opt_y = String::from(",transform=tfy");
+        if self.text_color != "" {
+            write!(&mut opt_x, ",color='{}'", self.text_color).unwrap();
+            write!(&mut opt_y, ",color='{}'", self.text_color).unwrap();
         }
         if self.fontsize > 0.0 {
             write!(&mut opt_x, ",fontsize={}", self.fontsize).unwrap();
-        }
-        opt_y.push_str(&opt_x);
-        if self.offset_v > 0.0 || self.txt_offset_v > 0.0 {
-            write!(&mut opt_x, ",transform=tfx").unwrap();
-        }
-        if self.offset_v > 0.0 || self.txt_offset_h > 0.0 {
-            write!(&mut opt_y, ",transform=tfy").unwrap();
+            write!(&mut opt_y, ",fontsize={}", self.fontsize).unwrap();
         }
         (opt_x, opt_y)
     }
@@ -339,15 +355,15 @@ mod tests {
         assert_eq!(indicator.flipped, false);
         assert_eq!(indicator.log_x, false);
         assert_eq!(indicator.log_y, false);
-        assert_eq!(indicator.color.len(), 7);
+        assert_eq!(indicator.edge_color.len(), 7);
         assert_eq!(indicator.line_style.len(), 0);
         assert_eq!(indicator.line_width, 0.0);
         assert_eq!(indicator.offset_v, 5.0);
         assert_eq!(indicator.no_text, false);
         assert_eq!(indicator.fontsize, 0.0);
-        assert_eq!(indicator.txt_slope.len(), 0);
-        assert_eq!(indicator.txt_offset_h, 3.0);
-        assert_eq!(indicator.txt_offset_v, 3.0);
+        assert_eq!(indicator.text_slope.len(), 0);
+        assert_eq!(indicator.text_offset_h, 3.0);
+        assert_eq!(indicator.text_offset_v, 3.0);
         assert_eq!(indicator.buffer.len(), 0);
     }
 
@@ -385,8 +401,8 @@ mod tests {
         let mut indicator = SlopeIndicator::new();
 
         indicator.set_offset_v(7.0);
-        indicator.set_txt_offset_h(1.0);
-        indicator.set_txt_offset_v(3.0);
+        indicator.set_text_offset_h(1.0);
+        indicator.set_text_offset_v(3.0);
         indicator.set_flipped(false);
         assert_eq!(
             indicator.transform_text(1.0),
@@ -443,7 +459,7 @@ mod tests {
     fn options_works() {
         let mut indicator = SlopeIndicator::new();
         indicator
-            .set_color("red")
+            .set_edge_color("red")
             .set_line_style("--")
             .set_line_width(2.0)
             .set_offset_v(12.0);
@@ -460,7 +476,7 @@ mod tests {
     #[test]
     fn draw_works() {
         let mut indicator = SlopeIndicator::new();
-        indicator.draw(1.0, 0.5, 0.5, 0.2);
+        indicator.draw(1.0, 0.5, 0.5);
         let b: &str = "x=np.array([0.4,0.6,0.6,0.4,],dtype=float)\n\
                        y=np.array([0.4,0.4,0.6,0.4,],dtype=float)\n\
                        tf=tra.offset_copy(plt.gca().transData,fig=plt.gcf(),x=0,y=-5,units='points')\n\
--- tests/test_slope_indicator.rs
+++ tests/test_slope_indicator.rs
@@ -1,4 +1,5 @@
 use plotpy::{Curve, Plot, SlopeIndicator};
+use russell_lab::Vector;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
@@ -10,8 +11,13 @@ fn test_slope_indicator() -> Result<(), &'static str> {
     // curves
     let mut curve1 = Curve::new();
     let mut curve2 = Curve::new();
-    curve1.draw(&[-1.0, 1.0], &[-1.0, 1.0]);
-    curve2.draw(&[-1.0, 1.0], &[1.0, -1.0]);
+    let slope = 1.0 / 3.0;
+    let x = [-10.0, 10.0];
+    let dx = x[1] - x[0];
+    let y1 = [5.0, 5.0 + slope * dx];
+    let y2 = [5.0 + slope * dx, 5.0];
+    curve1.draw(&x, &y1);
+    curve2.draw(&x, &y2);
 
     // indicator object and options
     let mut indicator1 = SlopeIndicator::new();
@@ -24,20 +30,32 @@ fn test_slope_indicator() -> Result<(), &'static str> {
     let mut indicator8 = SlopeIndicator::new();
 
     // configure indicators
-    indicator2.set_flipped(true);
-    indicator4.set_flipped(true);
-    indicator6.set_flipped(true);
-    indicator8.set_flipped(true);
+    indicator1.set_precision(3).set_offset_v(1.0);
+    indicator2.set_precision(3).set_offset_v(1.0).set_flipped(true);
+    indicator3.set_precision(3).set_offset_v(1.0);
+    indicator4.set_precision(3).set_offset_v(1.0).set_flipped(true);
+    indicator5.set_precision(3).set_offset_v(1.0);
+    indicator6.set_precision(3).set_offset_v(1.0).set_flipped(true);
+    indicator7.set_precision(3).set_offset_v(1.0);
+    indicator8.set_precision(3).set_offset_v(1.0).set_flipped(true);
 
     // draw indicator
-    indicator1.draw(1.0, -0.5, -0.5, 0.25);
-    indicator2.draw(1.0, -0.5, -0.5, 0.25);
-    indicator3.draw(1.0, 0.5, 0.5, 0.25);
-    indicator4.draw(1.0, 0.5, 0.5, 0.25);
-    indicator5.draw(-1.0, -0.5, 0.5, 0.25);
-    indicator6.draw(-1.0, -0.5, 0.5, 0.25);
-    indicator7.draw(-1.0, 0.5, -0.5, 0.25);
-    indicator8.draw(-1.0, 0.5, -0.5, 0.25);
+    let xc = x[0] + dx / 4.0;
+    let yc = y1[0] + slope * dx / 4.0;
+    indicator1.draw(slope, xc, yc);
+    indicator2.draw(slope, xc, yc);
+    let xc = x[0] + 3.0 * dx / 4.0;
+    let yc = y1[0] + slope * 3.0 * dx / 4.0;
+    indicator3.draw(slope, xc, yc);
+    indicator4.draw(slope, xc, yc);
+    let xc = x[0] + dx / 4.0;
+    let yc = y2[0] - slope * dx / 4.0;
+    indicator5.draw(-slope, xc, yc);
+    indicator6.draw(-slope, xc, yc);
+    let xc = x[0] + 3.0 * dx / 4.0;
+    let yc = y2[0] - slope * 3.0 * dx / 4.0;
+    indicator7.draw(-slope, xc, yc);
+    indicator8.draw(-slope, xc, yc);
 
     // add features to plot
     let mut plot = Plot::new();
@@ -54,12 +72,111 @@ fn test_slope_indicator() -> Result<(), &'static str> {
 
     // save figure
     let path = Path::new(OUT_DIR).join("integ_slope_indicator.svg");
-    plot.save(&path)?;
+    plot.set_equal_axes(true).grid_and_labels("x", "y").save(&path)?;
 
     // check number of lines
     let file = File::open(path).map_err(|_| "cannot open file")?;
     let buffered = BufReader::new(file);
     let lines_iter = buffered.lines();
-    assert!(lines_iter.count() > 630);
+    assert!(lines_iter.count() > 770);
+    Ok(())
+}
+
+#[test]
+fn test_slope_indicator_log() -> Result<(), &'static str> {
+    // linear models on log-log
+    let (p, slope) = (5.0, 2.0);
+    let (x0, y0) = (10.0, 100.0);
+    let (lx0, ly0) = (f64::log10(x0), f64::log10(y0));
+    let ly1 = |lx: f64| ly0 + slope * (lx - lx0);
+    let f1 = |x: f64| f64::powf(10.0, ly1(f64::log10(x)));
+    let xmax = x0 + f64::powf(10.0, p);
+    let ymax = f1(xmax);
+    let ly00 = f64::log10(ymax);
+    let ly2 = |lx: f64| ly00 - slope * (lx - lx0);
+    let f2 = |x: f64| f64::powf(10.0, ly2(f64::log10(x)));
+
+    // curves
+    let mut curve1 = Curve::new();
+    let mut curve2 = Curve::new();
+    curve1.set_marker_style("o");
+    curve2.set_marker_style("*");
+    let x = Vector::linspace(x0, xmax, 5);
+    let y1 = x.get_mapped(f1);
+    let y2 = x.get_mapped(f2);
+    curve1.draw(&x, &y1);
+    curve2.draw(&x, &y2);
+
+    // indicator object and options
+    let mut indicator1 = SlopeIndicator::new();
+    let mut indicator2 = SlopeIndicator::new();
+    let mut indicator3 = SlopeIndicator::new();
+    let mut indicator4 = SlopeIndicator::new();
+    let mut indicator5 = SlopeIndicator::new();
+    let mut indicator6 = SlopeIndicator::new();
+    let mut indicator7 = SlopeIndicator::new();
+    let mut indicator8 = SlopeIndicator::new();
+
+    // configure indicators
+    indicator1.set_offset_v(0.0).set_log_x(true).set_log_y(true);
+    indicator2
+        .set_offset_v(0.0)
+        .set_log_x(true)
+        .set_log_y(true)
+        .set_flipped(true);
+    indicator3.set_offset_v(0.0).set_log_x(true).set_log_y(true);
+    indicator4
+        .set_offset_v(0.0)
+        .set_log_x(true)
+        .set_log_y(true)
+        .set_flipped(true);
+    indicator5.set_offset_v(0.0).set_log_x(true).set_log_y(true);
+    indicator6
+        .set_offset_v(0.0)
+        .set_log_x(true)
+        .set_log_y(true)
+        .set_flipped(true);
+    indicator7.set_offset_v(0.0).set_log_x(true).set_log_y(true);
+    indicator8
+        .set_offset_v(0.0)
+        .set_log_x(true)
+        .set_log_y(true)
+        .set_flipped(true);
+
+    // draw indicator
+    println!("{},{}", 1e2, f2(1e2));
+    indicator1.draw(slope, 1e2, f1(1e2));
+    indicator2.draw(slope, 1e2, f1(1e2));
+    indicator3.draw(slope, 1e4, f1(1e4));
+    indicator4.draw(slope, 1e4, f1(1e4));
+    indicator5.draw(-slope, 1e2, f2(1e2));
+    indicator6.draw(-slope, 1e2, f2(1e2));
+    indicator7.draw(-slope, 1e4, f2(1e4));
+    indicator8.draw(-slope, 1e4, f2(1e4));
+
+    // add features to plot
+    let mut plot = Plot::new();
+    plot.set_log_x(true)
+        .set_log_y(true)
+        .add(&curve1)
+        .add(&curve2)
+        .add(&indicator1)
+        .add(&indicator2)
+        .add(&indicator3)
+        .add(&indicator4)
+        .add(&indicator5)
+        .add(&indicator6)
+        .add(&indicator7)
+        .add(&indicator8);
+
+    // save figure
+    let path = Path::new(OUT_DIR).join("integ_slope_indicator_log.svg");
+    plot.set_equal_axes(true).grid_and_labels("x", "y").save(&path)?;
+
+    // check number of lines
+    let file = File::open(path).map_err(|_| "cannot open file")?;
+    let buffered = BufReader::new(file);
+    let lines_iter = buffered.lines();
+    assert!(lines_iter.count() > 610);
     Ok(())
 }

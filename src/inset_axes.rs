79d535ff5eedd49fb60e18f94c2115dffaccc3ca
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-02-09 10:54:06 +1000

    Improve InsetAxes

--- src/inset_axes.rs
+++ src/inset_axes.rs
@@ -3,135 +3,105 @@ use std::fmt::Write;
 
 /// Implements the capability to add inset Axes to existing Axes.
 pub struct InsetAxes {
-    handle: String,           // Python's variable name
-    xlim: Option<(f64, f64)>, // range for x
-    ylim: Option<(f64, f64)>, // range for y
-    extra: String,            // extra commands (comma separated)
-    buffer: String,           // buffer
+    xmin: f64,
+    xmax: f64,
+    ymin: f64,
+    ymax: f64,
+    extra_for_axes: String,
+    extra_for_indicator: String,
+    buffer: String,
 }
 
 impl InsetAxes {
     /// Creates a new `InsetAxes` object with an empty buffer.
     ///
-    /// # Arguments
-    ///
-    /// * `handle` - The name of the **Python variable** that will hold the inset Axes.
-    ///
     /// # Returns
     ///
     /// A new instance of `InsetAxes`.
-    pub fn new(handle: &str) -> Self {
+    pub fn new() -> Self {
         Self {
-            handle: handle.to_string(),
-            xlim: None,
-            ylim: None,
-            extra: String::new(),
+            xmin: 0.0,
+            xmax: 1.0,
+            ymin: 0.0,
+            ymax: 1.0,
+            extra_for_axes: String::new(),
+            extra_for_indicator: String::new(),
             buffer: String::new(),
         }
     }
 
-    /// Draws the `InsetAxes` onto the current Axes.
-    ///
-    /// This function generates a command to create an inset Axes within the current Axes.
-    /// The command is stored in the buffer.
-    ///
-    /// **Note::** The `handle` is the name of the **Python variable** that will hold the inset Axes.
-    ///
-    /// # Arguments
-    ///
-    /// * `x0` - The x-coordinate of the lower-left corner of the inset Axes.
-    /// * `y0` - The y-coordinate of the lower-left corner of the inset Axes.
-    /// * `width` - The width of the inset Axes.
-    /// * `height` - The height of the inset Axes.
-    ///
-    /// The bounds are `(x0, y0, width, height)` where `x0`, `y0` are the lower-left corner of the inset Axes,
-    /// and `width`, `height` are the width and height of the inset Axes.
-    ///
-    /// # Returns
-    ///
-    /// A mutable reference to the `InsetAxes` instance, allowing for method chaining.
-    pub fn draw(&mut self, x0: f64, y0: f64, width: f64, height: f64) -> &mut Self {
-        let opt = self.options();
-        write!(
-            &mut self.buffer,
-            "{} = plt.gca().inset_axes([{},{},{},{}]{})\n",
-            self.handle, x0, y0, width, height, opt
-        )
-        .unwrap();
+    /// Adds new graph entity
+    pub fn add(&mut self, graph: &dyn GraphMaker) -> &mut Self {
+        let buf0 = graph.get_buffer();
+        let buf1 = buf0.replace("plt.gca()", "zoom");
+        let buf2 = buf1.replace("plt.imshow", "zoom.imshow");
+        self.buffer.push_str(&buf2);
         self
     }
 
-    /// Sets the x-range for the inset Axes.
+    /// Draws the inset Axes.
+    ///
+    /// Example of normalized coordinates: `(0.5, 0.5, 0.4, 0.3)`.
     ///
     /// # Arguments
     ///
-    /// * `xmin` - The minimum x-value.
-    /// * `xmax` - The maximum x-value.
-    ///
-    /// # Returns
-    ///
-    /// A mutable reference to the `InsetAxes` instance, allowing for method chaining.
-    pub fn set_xlim(&mut self, xmin: f64, xmax: f64) -> &mut Self {
-        self.xlim = Some((xmin, xmax));
+    /// * `u0` -- The normalized (0 to 1) horizontal figure coordinate of the lower-left corner of the inset Axes.
+    /// * `v0` -- The normalized (0 to 1) vertical figure coordinate of the lower-left corner of the inset Axes.
+    /// * `width` -- The width of the inset Axes.
+    /// * `height` -- The height of the inset Axes.
+    pub fn draw(&mut self, u0: f64, v0: f64, width: f64, height: f64) {
+        let opt1 = self.options_for_axes();
+        let opt2 = self.options_for_indicator();
+        self.buffer.insert_str(
+            0,
+            &format!(
+                "zoom=plt.gca().inset_axes([{},{},{},{}],xlim=({},{}),ylim=({},{}){})\n",
+                u0, v0, width, height, self.xmin, self.xmax, self.ymin, self.ymax, opt1,
+            ),
+        );
+        write!(&mut self.buffer, "plt.gca().indicate_inset_zoom(zoom{})\n", opt2,).unwrap();
+    }
+
+    /// Sets the limits of axes
+    pub fn set_range(&mut self, xmin: f64, xmax: f64, ymin: f64, ymax: f64) -> &mut Self {
+        self.xmin = xmin;
+        self.xmax = xmax;
+        self.ymin = ymin;
+        self.ymax = ymax;
         self
     }
 
-    /// Sets the y-range for the inset Axes.
+    /// Sets extra Matplotlib commands for the inset Axes (comma separated).
     ///
-    /// # Arguments
-    ///
-    /// * `ymin` - The minimum y-value.
-    /// * `ymax` - The maximum y-value.
-    ///
-    /// # Returns
-    ///
-    /// A mutable reference to the `InsetAxes` instance, allowing for method chaining.
-    pub fn set_ylim(&mut self, ymin: f64, ymax: f64) -> &mut Self {
-        self.ylim = Some((ymin, ymax));
+    /// [See Matplotlib's documentation for extra parameters](<https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.html#matplotlib.axes.Axes>)
+    pub fn set_extra_for_axes(&mut self, extra: &str) -> &mut Self {
+        self.extra_for_axes = extra.to_string();
         self
     }
 
-    /// Sets extra Matplotlib commands (comma separated).
+    /// Sets extra Matplotlib commands for the indicator (comma separated).
     ///
-    /// # Arguments
-    ///
-    /// * `extra` - A string containing extra Matplotlib commands, separated by commas.
-    ///
-    /// **Important:** The extra commands must be comma separated.
-    ///
-    /// [See Matplotlib's documentation for extra parameters](https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.inset_axes.html#matplotlib.axes.Axes.inset_axes)
-    ///
-    /// # Returns
-    ///
-    /// A mutable reference to the `InsetAxes` instance, allowing for method chaining.
-    pub fn set_extra(&mut self, extra: &str) -> &mut Self {
-        self.extra = extra.to_string();
+    /// [See Matplotlib's documentation for extra parameters](https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.indicate_inset.html#matplotlib.axes.Axes.indicate_inset)
+    pub fn set_extra_for_indicator(&mut self, extra: &str) -> &mut Self {
+        self.extra_for_indicator = extra.to_string();
         self
     }
 
-    /// Returns options for the `InsetAxes`.
-    ///
-    /// This function generates a string containing the options for the `InsetAxes`,
-    /// including the x-range, y-range, and any extra commands.
-    ///
-    /// # Returns
-    ///
-    /// A string containing the options for the `InsetAxes`.
-    fn options(&self) -> String {
+    /// Returns options for the inset Axes
+    fn options_for_axes(&self) -> String {
         let mut opt = String::new();
-
-        if let Some((xmin, xmax)) = self.xlim {
-            write!(&mut opt, ",xlim=({},{})", xmin, xmax).unwrap();
+        if !self.extra_for_axes.is_empty() {
+            write!(&mut opt, ",{}", self.extra_for_axes).unwrap();
         }
+        opt
+    }
 
-        if let Some((ymin, ymax)) = self.ylim {
-            write!(&mut opt, ",ylim=({},{})", ymin, ymax).unwrap();
+    /// Returns options for the indicator
+    fn options_for_indicator(&self) -> String {
+        let mut opt = String::new();
+        if !self.extra_for_indicator.is_empty() {
+            write!(&mut opt, ",{}", self.extra_for_indicator).unwrap();
         }
-
-        if !self.extra.is_empty() {
-            write!(&mut opt, ",{}", self.extra).unwrap();
-        }
-
         opt
     }
 }
@@ -158,54 +128,4 @@ impl GraphMaker for InsetAxes {
 mod tests {
     use super::InsetAxes;
     use crate::GraphMaker;
-
-    #[test]
-    fn new_works() {
-        let inset = InsetAxes::new("zoom");
-        assert_eq!(inset.handle, "zoom");
-        assert_eq!(inset.get_buffer(), "");
-    }
-
-    #[test]
-    fn draw_works() {
-        let mut inset = InsetAxes::new("zoom");
-        inset.draw(0.1, 0.1, 0.4, 0.4);
-        assert_eq!(inset.get_buffer(), "zoom = plt.gca().inset_axes([0.1,0.1,0.4,0.4])\n");
-    }
-
-    #[test]
-    fn set_xlim_works() {
-        let mut inset = InsetAxes::new("zoom");
-        inset.set_xlim(8.0, 9.0);
-        assert_eq!(inset.options(), ",xlim=(8,9)");
-    }
-
-    #[test]
-    fn set_ylim_works() {
-        let mut inset = InsetAxes::new("zoom");
-        inset.set_ylim(3.0, 5.0);
-        assert_eq!(inset.options(), ",ylim=(3,5)");
-    }
-
-    #[test]
-    fn set_extra_works() {
-        let mut inset = InsetAxes::new("zoom");
-        inset.set_extra("aspect='equal'");
-        assert_eq!(inset.options(), ",aspect='equal'");
-    }
-
-    #[test]
-    fn options_combined_works() {
-        let mut inset = InsetAxes::new("zoom");
-        inset.set_xlim(1.0, 2.0).set_ylim(3.0, 4.0).set_extra("aspect='equal'");
-        assert_eq!(inset.options(), ",xlim=(1,2),ylim=(3,4),aspect='equal'");
-    }
-
-    #[test]
-    fn clear_buffer_works() {
-        let mut inset = InsetAxes::new("zoom");
-        inset.draw(0.1, 0.1, 0.4, 0.4);
-        inset.clear_buffer();
-        assert_eq!(inset.get_buffer(), "");
-    }
 }
--- tests/test_inset_axes.rs
+++ tests/test_inset_axes.rs
@@ -6,31 +6,8 @@ use std::path::Path;
 const OUT_DIR: &str = "/tmp/plotpy/integ_tests";
 
 #[test]
-fn test_inset_axes() -> Result<(), StrError> {
-    // object and options
-    let mut inset = InsetAxes::new("zoom");
-
+fn test_inset_axes_1() -> Result<(), StrError> {
     // draw image
-    let mut plot = Plot::new();
-    draw_image(&mut plot);
-
-    // add inset axes
-    inset.set_xlim(0.0, 1.0).set_ylim(6.0, 5.0).draw(3.0, 4.0, 2.0, 2.0);
-    plot.add(&inset);
-
-    // save figure
-    let path = Path::new(OUT_DIR).join("integ_inset_axes.svg");
-    plot.set_show_errors(true).save(&path)?;
-
-    // check number of lines
-    let file = File::open(path).map_err(|_| "cannot open file")?;
-    let buffered = BufReader::new(file);
-    let lines_iter = buffered.lines();
-    assert!(lines_iter.count() > 0);
-    Ok(())
-}
-
-fn draw_image(plot: &mut Plot) {
     let data = [
         [0.8, 2.4, 2.5, 3.9, 0.0, 4.0, 0.0],
         [2.4, 0.0, 4.0, 1.0, 2.7, 0.0, 0.0],
@@ -41,6 +18,30 @@ fn draw_image(plot: &mut Plot) {
         [0.1, 2.0, 0.0, 1.4, 0.0, 1.9, 6.3],
     ];
     let mut img = Image::new();
+    let mut plot = Plot::new();
     img.set_colormap_name("terrain").set_extra("alpha=0.8").draw(&data);
     plot.add(&img);
+
+    // inset axes
+    let mut inset = InsetAxes::new();
+    inset
+        .set_extra_for_axes("xticklabels=[],yticklabels=[]")
+        .set_extra_for_indicator("edgecolor='red',linewidth=2")
+        .set_range(0.0, 1.0, 5.0, 6.0);
+    inset.add(&img).draw(0.5, 0.5, 0.4, 0.3);
+
+    // add entities to plot
+    plot.add(&img).add(&inset);
+
+    // save figure
+    let path = Path::new(OUT_DIR).join("integ_inset_axes_1.svg");
+    plot.set_show_errors(true).save(&path)?;
+
+    // check number of lines
+    let file = File::open(path).map_err(|_| "cannot open file")?;
+    let buffered = BufReader::new(file);
+    let lines_iter = buffered.lines();
+    let n = lines_iter.count().clone();
+    assert!(n > 510 && n < 600);
+    Ok(())
 }

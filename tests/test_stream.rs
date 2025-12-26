64934b608be15ac7f58294591ca44d642e916984
Dorival Pedroso <dorival.pedroso@gmail.com>
2025-12-27 08:51:38 +1000

    Impl draw streamlines and arrows using 1d arrays for the coordinates

--- tests/test_stream.rs
+++ tests/test_stream.rs
@@ -1,4 +1,4 @@
-use plotpy::{generate2d, Plot, StrError, Stream};
+use plotpy::{generate2d, linspace, Plot, StrError, Stream};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
@@ -14,13 +14,13 @@ fn test_stream_arrows_1() -> Result<(), StrError> {
     // data
     let (nx, ny) = (10, 10);
     let (xx, yy) = generate2d(-2.0, 2.0, -2.0, 2.0, nx, ny);
-    let (mut dx, mut dy) = generate2d(0.0, 1.0, 0.0, 1.0, nx, ny);
+    let (mut uu, mut vv) = generate2d(0.0, 1.0, 0.0, 1.0, nx, ny);
     for j in 0..ny {
         for i in 0..nx {
             let x = xx[j][i];
             let y = yy[j][i];
-            dx[j][i] = -y;
-            dy[j][i] = x;
+            uu[j][i] = -y;
+            vv[j][i] = x;
         }
     }
 
@@ -31,12 +31,12 @@ fn test_stream_arrows_1() -> Result<(), StrError> {
         .set_streamplot_arrow_style("fancy")
         .set_streamplot_density(0.8)
         .set_streamplot_extra("broken_streamlines=False")
-        .draw(&xx, &yy, &dx, &dy);
+        .draw(&xx, &yy, &uu, &vv);
     quiver
         .set_color("#4752c7ff")
         .set_quiver_inv_scale(15.0)
         .set_quiver_pivot("mid")
-        .draw_arrows(&xx, &yy, &dx, &dy);
+        .draw_arrows(&xx, &yy, &uu, &vv);
 
     // add contour to plot
     let mut plot = Plot::new();
@@ -52,5 +52,44 @@ fn test_stream_arrows_1() -> Result<(), StrError> {
     let lines_iter = buffered.lines();
     let n = lines_iter.count();
     assert!(n > 8650 && n < 8730);
+    Ok(())
+}
+
+#[test]
+fn test_stream_arrows_2() -> Result<(), StrError> {
+    // object and options
+    let mut stream = Stream::new();
+    let mut quiver = Stream::new();
+
+    // data
+    let n = 10;
+    let x = linspace(1.0, 2.0, n);
+    let y = x.clone();
+    let u = linspace(1.0, 2.0, n);
+    let v = u.clone();
+    let (uu, vv) = generate2d(1.0, 2.0, 1.0, 2.0, n, n);
+
+    // draw arrows
+    stream.set_streamline_zorder(1).draw_alt(&x, &y, &uu, &vv);
+    quiver
+        .set_quiver_zorder(2)
+        .set_color("red")
+        .set_quiver_inv_scale(20.0)
+        .draw_arrows_alt(&x, &y, &u, &v);
+
+    // add contour to plot
+    let mut plot = Plot::new();
+    plot.add(&stream).add(&quiver);
+
+    // save figure
+    let path = Path::new(OUT_DIR).join("integ_stream_arrows_2.svg");
+    plot.set_equal_axes(true).save(&path)?;
+
+    // check number of lines
+    let file = File::open(path).map_err(|_| "cannot open file")?;
+    let buffered = BufReader::new(file);
+    let lines_iter = buffered.lines();
+    let n = lines_iter.count();
+    assert!(n > 3600 && n < 3680);
     Ok(())
 }
